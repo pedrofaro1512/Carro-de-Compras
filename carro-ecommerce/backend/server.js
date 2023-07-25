1ca417de94c03dccca3a7a2027e22cc6d1f3cfdc
Pedro Romero <108835699+pedrofaro1512@users.noreply.github.com>
2023-07-25 18:33:19 -0500

    Adición de petición a API propia.

---
+++ carro-ecommerce/backend/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 3001;
+
+// Middleware para habilitar CORS
+app.use(cors());
+
+// Ruta para redirigir las solicitudes a la API externa
+app.get("/api", async (req, res) => {
+  try {
+    const response = await axios.get("https://api.jsonserve.com/ddZaSD");
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener los datos" });
+  }
+});
+
+// Inicia el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
--- carro-ecommerce/src/App.js
+++ carro-ecommerce/src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
 import TopMenu from "./components/TopMenu/TopMenu";
+import useFetch from "./hooks/useFetch";
+import { urlApiProducts } from "./utils/constants";
 
 function App() {
+  const result = useFetch(urlApiProducts);
+
+  console.log(result);
   return (
     <div>
       <TopMenu />
